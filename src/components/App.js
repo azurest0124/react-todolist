8a7c3bdf3f761a3146dc823ee218bea5cfb363e1
azurest0124 <azurest0124@naver.com>
2018-12-26 17:40:13 +0900

    snapshot - UI(C), FUNC(ing)

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,25 @@ import React, { Component } from "react";
 import PageTemplate from "./PageTemplate";
 import TodoInput from "./TodoInput";
 import TodoList from "./TodoList";
+
 class App extends Component {
+  state = {
+    input: ""
+  };
+
+  handleChange = e => {
+    const { value } = e.target;
+    this.setState({
+      input: value
+    });
+  };
+
   render() {
+    const { input } = this.state;
+    const { handleChange } = this;
     return (
       <PageTemplate>
-        <TodoInput />
+        <TodoInput onChange={handleChange} value={input} />
         <TodoList />
       </PageTemplate>
     );
